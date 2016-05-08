fa138e457aa14c7369939d6ac28b7c8bfc135c2e
Haoquan Li <haoqli.cs@gmail.com>
2016-05-08 19:15:16 -0400

    submit for p.a. 9

---
+++ Programming Assignment 9/routes.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/locations', 'LocationController@index')->name('locations');
+Route::get('/stories',  'StoryController@index')->name('stories');
+Route::get('/tags', 'StoryController@tags')->name('tags');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/story/create', 'StoryController@create')->name('create');
+Route::post('/story/store', 'StoryController@store')->name('store');
+Route::get('/api/stories',  'StoryController@api_stories')->name('stories');
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
